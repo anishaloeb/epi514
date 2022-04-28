00c581a612018de71354ee7cc9aedfb69e174e80
bbryer <bbryer@uw.edu>
2022-04-28 15:51:30 -0700

    Test Test

--- Table1.R
+++ Table1.R
@@ -44,7 +44,7 @@ table(Caregive18$EMPLOY1)
 
 # Employment
 
-library(usethis)
-usethis::use_git_config(user.name = "anishaloeb", user.email = "aloeb@uw.edu")
-usethis::create_github_token() 
-credentials::set_github_pat("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")
+#library(usethis)
+#usethis::use_git_config(user.name = "anishaloeb", user.email = "aloeb@uw.edu")
+#usethis::create_github_token() 
+#credentials::set_github_pat("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")
