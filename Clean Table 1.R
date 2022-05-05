52edad05b2f3b6d48d3ea24b2623a7573d2c330b
bbryer <104479398+bbryer@users.noreply.github.com>
2022-05-05 15:07:24 -0700

    Update Clean Table 1.R

--- Clean Table 1.R
+++ Clean Table 1.R
@@ -48,3 +48,76 @@ keepVars <- c("X_AGE65YR", "X_STATE", "X_PSU", "MENTHLTH", "HLTHPLN1", "CHECKUP1
 BRFSS2018_append <- BRFSS2018_append[, keepVars]
 write.csv(BRFSS2018_append, "Desktop/BRFSS2018_appendv2.csv", row.names = FALSE)
 
+# Ben Bryer
+# EPI514 Table 1
+# 5/2/22
+
+#load data
+rm(list = ls())
+setwd("C:/Users/bryer/OneDrive/Documents/UW/Spring22/EPI514")
+
+library("tidyverse")
+library("haven")
+library("boot")
+library("survey")
+
+BRFSS2018_append <- read.csv("BRFSS2018_appendv2.csv", stringsAsFactors=T)
+View(BRFSS2018_append)
+
+# Sep by Exp Status
+# Remove Missing for caregiving
+BRFSS2018_append$CAREGIV1[BRFSS2018_append$CAREGIV1 == 9] <- NA
+BRFSS2018_append$CAREGIV1[BRFSS2018_append$CAREGIV1 == 7] <- NA
+BRFSS2018_append$CAREGIV1[BRFSS2018_append$CAREGIV1 == 8] <- NA
+BRFSS2018_append$CAREGIV1[BRFSS2018_append$CAREGIV1 == ""] <- NA
+
+#Remove missing for variables
+BRFSS2018_append$X_EDUCAG[BRFSS2018_append$X_EDUCAG == 9] <- NA
+BRFSS2018_append$MARITAL[BRFSS2018_append$MARITAL == ""] <- NA
+BRFSS2018_append$MARITAL[BRFSS2018_append$MARITAL == 9] <- NA
+BRFSS2018_append$GENHLTH[BRFSS2018_append$GENHLTH == 7] <- NA
+BRFSS2018_append$GENHLTH[BRFSS2018_append$GENHLTH == 9] <- NA
+BRFSS2018_append$GENHLTH[BRFSS2018_append$GENHLTH == ""] <- NA
+
+
+#create variable for caregivers who have given care for more than 30 days
+
+#Try out new CareCat1
+BRFSS2018_append$CareCat<- NA
+BRFSS2018_append$CareCat[BRFSS2018_append$CAREGIV1 ==2] <- 0
+BRFSS2018_append$CareCat[BRFSS2018_append$CAREGIV1 ==1 & BRFSS2018_append$CRGVLNG1 == 1] <- 0
+BRFSS2018_append$CareCare[BRFSS2018_append$CAREGIV1 ==1 & BRFSS2018_append$CRGVLNG1 > 1] <- 1
+table(BRFSS2018_append$CareCat, useNA = "always") # raw
+
+
+
+options(survey.lonely.psu = "adjust")
+
+#create weighted data frame
+bd <- svydesign(data = BRFSS2018_append, id = ~X_PSU, strata = ~X_STSTR,
+                weight = ~X_LLCPWT, nest = TRUE)
+
+###Education 
+summary(BRFSS2018_append$X_EDUCAG)
+
+table(BRFSS2018_append$X_EDUCAG, BRFSS2018_append$CareCat, useNA = "always") # raw
+prop.table(svytable(~X_EDUCAG + CareCat, bd, exclude='null', na.action=na.pass), margin = 2) *100
+
+
+
+### Marital
+summary(BRFSS2018_append$MARITAL)
+
+table(BRFSS2018_append$MARITAL, BRFSS2018_append$CareCat, useNA = "always") # raw
+prop.table(svytable(~MARITAL + CareCat, bd, exclude='null', na.action=na.pass), margin = 2) *100
+
+###GenHlth
+summary(BRFSS2018_append$GENHLTH)
+
+table(BRFSS2018_append$GENHLTH, BRFSS2018_append$CareCat, useNA = "always") # raw
+prop.table(svytable(~GENHLTH + CareCat, bd, exclude='null', na.action=na.pass), margin = 2) *100
+
+
+
+
+
